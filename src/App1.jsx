4cde8bd5757d0e00b36eba2d643c487f91af4d8a
dvsoroka <dvsoroka@gmail.com>
2023-10-19 16:43:13 +0300

    Correcrt way of writing setter of useState

--- src/App1.jsx
+++ src/App1.jsx
@@ -1,46 +1,23 @@
 import React from "react"
 
 export default function App() {
-//  const result = React.useState("Yes")  
-// We can immediately destructure the array:
-//  const [result, func] = React.useState("Yes")
-
-
-/* Normally if we had a variable where we saving this, if we just said:
-    let isImportant = "Yes"     
-                            // then we already know we could simply say: 
-    isImportant = "No"
-                            // however when we're working with "state" we can't simply say: isImportant = "No"
-                            // instead we are given a function that allows us to make these changes so that React can handle 
-                            // those changes accordingly; there is a convention that function is called "set"+"Variable"
-
-    const [isImportant, func] = React.useState("Yes")
-*/
-    const [isImportant, setIsImportant] = React.useState("Yes") // and this setIsImportant is a function I can run and whatever value I provide to it
-                                                                // is going to be the new version of "state" for example: setIsImportant("No")
-//  setTimeout(() => setIsImportant("No"), 1500)
-
-
-/**
-     * Challenge: 
-     * 1. Create a function called `handleClick` that runs
-     *    setIsImportant("No")
-     * 2. add a click event listener to the div.state--value
-     *    that runs `handleClick` when the div is clicked.
-     */
     
-function handleClick() {
-    setIsImportant("No");
-    setTimeout(() => setIsImportant("YES!"), 3500)
-}
+    const [count, setCount] = React.useState(0)
 
+    function add() {
+        setCount(prevCount => prevCount + 1)
+    }
 
+    function subtract() {
+        setCount(prevCount => prevCount - 1)
+    }
     return (
-        <div className="state">
-            <h1 className="state--title">Is state important to know?</h1>
-            <div onClick={handleClick} className="state--value">
-                    <h1>{isImportant}</h1>
+        <div className="counter">
+            <button className="counter--minus" onClick={subtract}>–</button>
+            <div className="counter--count">
+                <h1>{count}</h1>
             </div>
+            <button className="counter--plus" onClick={add}>+</button>
         </div>
     )
 }
---
+++ src/App1_sav_1.jsx
@@ -0,0 +1,90 @@
+import React from "react"
+
+export default function App() {
+//  const result = React.useState("Yes")  
+// We can immediately destructure the array:
+//  const [result, func] = React.useState("Yes")
+
+
+/* Normally if we had a variable where we saving this, if we just said:
+    let isImportant = "Yes"     
+                            // then we already know we could simply say: 
+    isImportant = "No"
+                            // however when we're working with "state" we can't simply say: isImportant = "No"
+                            // instead we are given a function that allows us to make these changes so that React can handle 
+                            // those changes accordingly; there is a convention that function is called "set"+"Variable"
+
+    const [isImportant, func] = React.useState("Yes")
+*/
+    const [isImportant, setIsImportant] = React.useState("Yes") // and this setIsImportant is a function I can run and whatever value I provide to it
+                                                                // is going to be the new version of "state" for example: setIsImportant("No")
+//  setTimeout(() => setIsImportant("No"), 1500)
+
+
+/**
+     * Challenge: 
+     * 1. Create a function called `handleClick` that runs
+     *    setIsImportant("No")
+     * 2. add a click event listener to the div.state--value
+     *    that runs `handleClick` when the div is clicked.
+     
+function handleClick() {
+    setIsImportant("No");
+    setTimeout(() => setIsImportant("YES!"), 3500)
+}
+
+return (
+    <div className="state">
+        <h1 className="state--title">Is state important to know?</h1>
+        <div onClick={handleClick} className="state--value">
+                <h1>{isImportant}</h1>
+        </div>
+    </div>
+)
+
+*/
+    /**
+     * Challenge: Set up state to track our count (initial value is 0)
+     */
+    
+    const [count, setCount] = React.useState(0)
+
+    /**
+     * Note: if you ever need the old value of state
+     * to help you determine the new value of state,
+     * you should pass a callback function to your
+     * state setter function instead of using
+     * state directly. This callback function will
+     * receive the old value of state as its parameter,
+     * which you can then use to determine your new
+     * value of state.
+     */
+    function increaseCount() {
+//      setCount((count) => count+1)
+//      setCount(count+1)
+/*      setCount(function(oldValue) {
+            return oldValue + 1 
+        })
+*/
+        setCount(prevCount => prevCount + 1)
+    }
+
+    function decreaseCount() {
+//        setCount((count) => count-1)
+        setCount(count-1)
+    }
+
+
+
+    return (
+        <div className="counter">
+            <button className="counter--minus" onClick={decreaseCount}>–</button>
+            <div className="counter--count">
+                <h1>{count}</h1>
+            </div>
+            <button className="counter--plus" onClick={increaseCount}>+</button>
+        </div>
+    )
+
+    
+}
