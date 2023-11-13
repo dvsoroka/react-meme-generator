30fe1a31ec857a65be7df35eea0267345fc38b40
dvsoroka <dvsoroka@gmail.com>
2023-11-13 13:55:47 +0200

    adding radio buttons to React Fom

--- forms_examples/Form.jsx
+++ forms_examples/Form.jsx
@@ -18,11 +18,12 @@ to determine which property of that state object we should be updating.
             lastName: "", 
             email: "", 
             comments: "",
-            isFriendly: true
+            isFriendly: true,
+            employment: ""
         }
     )
 
-    console.log(formData)
+    console.log(formData.employment)
 
     function handleChange(event) {              // Before when we were simply updating a STRING we didn't care about what the previous version of 
 //      console.log(event.target.name)          // that string was because we were just going to overwrite it with whatever was in the input box.
@@ -105,6 +106,46 @@ to determine which property of that state object we should be updating.
                 name="isFriendly"
             />
             <label htmlFor="isFriendly">Are you friendly?</label>
+            <br />
+            <br />
+
+            <fieldset>
+                <legend>Current employment status</legend>
+                
+                <input 
+                    type="radio"
+                    id="unemployed"
+                    name="employment"
+                    value="unemployed"
+                    checked={formData.employment === "unemployed"} // https://scrimba.com/learn/learnreact/forms-in-react-radio-buttons-co14c423dbc2026a7a2b997a2
+                    onChange={handleChange}
+                />
+                <label htmlFor="unemployed">Unemployed</label>
+                <br />
+                
+                <input 
+                    type="radio"
+                    id="part-time"
+                    name="employment"
+                    value="part-time"
+                    checked={formData.employment === "part-time"}
+                    onChange={handleChange}
+                />
+                <label htmlFor="part-time">Part-time</label>
+                <br />
+                
+                <input 
+                    type="radio"
+                    id="full-time"
+                    name="employment"
+                    value="full-time"
+                    checked={formData.employment === "full-time"}
+                    onChange={handleChange}
+                />
+                <label htmlFor="full-time">Full-time</label>
+                <br />
+                
+            </fieldset>
             <br />
         </form>
     )
