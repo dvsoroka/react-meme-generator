9de8bec79d4f2a43a22d68396808f6aec596b23e
dvsoroka <dvsoroka@gmail.com>
2023-11-18 19:46:43 +0200

    WindowTracker should produce a memory leak Warning

--- window_tracker/App.jsx
+++ window_tracker/App.jsx
@@ -2,20 +2,34 @@ import React from "react"
 import WindowTracker from "./WindowTracker"
 
 export default function App() {
-    /**
+    /** https://scrimba.com/learn/learnreact/state-and-effect-practices-co3d647f0b559d6e65b6a2762
      * Challenge:
      * 1. Create state called `show`, default to `true`
      * 2. When the button is clicked, toggle `show`
      * 3. Only display `<WindowTracker>` if `show` is `true`
      */
     const [show, setShow] = React.useState(true)
+
+    function toggle() {
+        setShow(prevShow => !prevShow)
+    }
+
     console.log(show)
+
     return (
         <div className="container">
-            <button onClick={() => {setShow(prevState => !prevState)}}>
+            <button onClick={toggle}>
                 Toggle WindowTracker
             </button>
-            <WindowTracker />
+            {show  && <WindowTracker />}
         </div>
     )
 }
+
+/* When toggling off the "show" while trying to resize the window 
+ we are getting the following warning (actually NO in vite+ReactJs):
+ 
+Warning: Can't perform a React state update on an unmounted component. This is a no-op, but it indicates a memory leak in your application. To fix, cancel all subscriptions and asynchronous tasks in a useEffect cleanup function. at WindowTracker (exe1.bundle.js:69:56)
+
+this is what we call a "memory leak"
+*/
--- window_tracker/WindowTracker.jsx
+++ window_tracker/WindowTracker.jsx
@@ -1,7 +1,27 @@
 import React from "react"
 
 export default function WindowTracker() {
+    /**
+     * https://scrimba.com/learn/learnreact/state-and-effect-practices-co3d647f0b559d6e65b6a2762
+     * Challenge:
+     * 1. Create state called `windowWidth`, default to 
+     *    `window.innerWidth`
+     * 2. When the window width changes, update the state
+     * 3. Display the window width in the h1 so it updates
+     *    every time it changes
+     */
+    
+    const [windowWidth, setWindowWidth] = React.useState(window.innerWidth)
+
+    React.useEffect(() => {
+        window.addEventListener("resize", function() {
+            setWindowWidth(window.innerWidth)
+//          console.log("Resized!")
+        })
+    }, [])
+
     return (
-        <h1>Window width: {window.innerWidth}</h1>
+ //     <h1>Window width: {window.innerWidth}</h1>
+        <h1>Window width: {windowWidth}</h1>
     )
 }
